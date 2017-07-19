afaeb38098cff7e4858ee7829f13e94acfe177eb
AusWorley1 <austin.worley@ymail.com>
2017-07-19 04:45:09 +0000

    move ship, destroyed enemy ships

--- AusWorley1/Mars.java
+++ AusWorley1/Mars.java
@@ -25,13 +25,54 @@ public class Mars extends Ship {
     @Override
     public void doTurn(Arena arena) {
         // Fill in your strategy here
-        Coord coord = this.getCoord();
-        int x = coord.getX();
-     if (x < 5) {
-        this.move(arena, Direction.EAST);
-        List<Ship> nearby = this.getNearbyShips(arena);
-        Ship a = nearby.get(0);
-        System.out.println(a.getHealth());
-     }
+         List<Ship> nearby = this.getNearbyShips(arena);
+        Ship a;
+        //System.out.println(a.getHealth());
+        int size = nearby.size();
+        if (size>0) {
+            a = nearby.get(0);
+            Coord coord = this.getShipCoord(arena, a);
+            int shipX = coord.getX();
+            int shipY = coord.getY();
+            this.fire(arena, shipX, shipY);
+            this.fire(arena, shipX, shipY);
+            this.fire(arena, shipX, shipY);
+            this.move(arena, Direction.EAST);
+        }
+        else if (size == 0){
+            Coord coord = this.getCoord();
+            int x = coord.getX();
+            int maxX = arena.getXSize();
+            if (x == maxX){
+                this.move(arena, Direction.WEST);
+                
+            }else if (x < maxX){
+                this.move(arena, Direction.EAST);
+                
+            }
+            
+        }
+        
+        
+        
+    //     Coord coord = this.getCoord();
+    //     int x = coord.getX();
+    //  if (x < 5) {
+    //     this.move(arena, Direction.EAST);
+    //     List<Ship> nearby = this.getNearbyShips(arena);
+    //     Ship a = nearby.get(0);
+    //     System.out.println(a.getHealth());
+    //  }
     }
 }
+/*
+If enemy is in sight 
+    Shoot at ship with the least hull points
+    Move to the right
+When no enemy is in sight
+    Scan for enemy ships
+If at corner
+    Head one spots away from the corner
+If not at corner
+    Move towards corner
+*/
