c81cf72ac48459cc480e64661a4e922b8b75a23c
Nick <nlambros@hahwk.iit.edu>
2017-07-20 01:58:23 +0000

    team battle

--- Nickthegreat/MoistNoodle.java
+++ Nickthegreat/MoistNoodle.java
@@ -13,8 +13,8 @@ public class MoistNoodle extends Ship {
         this.initializeOwner("Nick");
         this.initializeHull(2);
         this.initializeFirepower(2);
-        this.initializeSpeed(2);
-        this.initializeRange(4);
+        this.initializeSpeed(1);
+        this.initializeRange(5);
     }
     
     /*
@@ -25,19 +25,48 @@ public class MoistNoodle extends Ship {
     @Override
     public void doTurn(Arena arena) {
         
+        Coord me = this.getCoord();
+    int a = me.getX();
+    int b = me.getY();
+    
+    
+    if (a > 5) {
+        this.move(arena, Direction.WEST);
+        
+    }
+    else if (a < 5) {
         this.move(arena, Direction.EAST);
-        List<Ship>nearby= this.getNearbyShips(arena);
-        
-        
-        //gets neaby enemies 
-        if(nearby.size()>0)     {
-        Ship first=nearby.get(0);
-        Coord coord=this.getShipCoord(arena, first);
-        int x=coord.getX();
-        int y=coord.getY();
+    }
+    
+        //gets neaby enemies
+        List<Ship> nearby = this.getNearbyShips(arena);
+// Loop over all the ships
+for (int i = 0; i < nearby.size(); i++) {
+    Ship ship = nearby.get(i);
+    // Call the getTeam() method on any ship to get its team name
+    String myTeam = this.getTeam();
+    String theirTeam = ship.getTeam();
+    // To compare Strings, we have to use the special .equals() method
+    // It will return true if the strings are equal and false if they are not
+    if (theirTeam.equals(myTeam)) {
+        // Don't shoot!
+    } else {
+        // In the new version of battleship, you can get any ship's coordinate, even if it is out of your range
+        // But, snce we used getNearbyShips(), all ships in this loop are in range
+        Coord coord = ship.getCoord();
+        int x = coord.getX();
+        int y = coord.getY();
+        // If you run out of firepower on a turn, you can still call fire(), but your ship won't actually fire
         this.fire(arena, x, y);
         this.fire(arena, x, y);
-        }
+    }
+}
+        
+        
+        
+        
+
+    
         
         
         }
