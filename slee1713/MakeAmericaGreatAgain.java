f8e6d943fdcf163742ce7bc69bb8b988c10725bd
Spencer Lee <slee216@hawk.iit.edu>
2017-07-20 01:59:32 +0000

    team battle

--- slee1713/MakeAmericaGreatAgain.java
+++ slee1713/MakeAmericaGreatAgain.java
@@ -11,10 +11,10 @@ public class MakeAmericaGreatAgain extends Ship {
     public MakeAmericaGreatAgain() {
         this.initializeName("Make America Great Again");
         this.initializeOwner("Your Name");
-        this.initializeHull(3);
-        this.initializeFirepower(3);
+        this.initializeHull(1);
+        this.initializeFirepower(1);
         this.initializeSpeed(1);
-        this.initializeRange(3);
+        this.initializeRange(7);
     }
     
     /*
@@ -22,11 +22,40 @@ public class MakeAmericaGreatAgain extends Ship {
      * @param arena (Arena) the battlefield for the match
      * @return void
      */
+     
     @Override
-    public void doTurn(Arena arena) {
-        //this.move(arena, Direction.EAST);
-        int i = arena.getRandom().nextInt(10);
-        if (i > 3) {
+    public void doTurn(Arena arena) { 
+        List<Ship> nearby = this.getNearbyShips(arena);
+    for (int i = 0; i < nearby.size(); i++) {
+        Ship ship = nearby.get(i);
+        String myTeam = this.getTeam();
+        String theirTeam = ship.getTeam();
+        if (theirTeam.equals(myTeam)) {
+        } else {
+            Coord coord = ship.getCoord();
+            int x = coord.getX();
+            int y = coord.getY();
+            this.fire(arena, x, y);
+            
+         this.move(arena, Direction.EAST);
+         this.fire(arena, 9, 1);
+        }
+    }
+         Coord me = this.getCoord();
+    int a = me.getX();
+    int b = me.getY();
+    
+    
+    if (a > 5) {
+        this.move(arena, Direction.WEST);
+        
+    }
+    else if (a < 5) {
+        this.move(arena, Direction.EAST);
+    }
+       // this.move(arena, Direction.EAST);
+        //int i = arena.getRandom().nextInt(10);
+        /*if (i > 3) {
             this.move(arena, Direction.NORTH);
         }
         else if (i > 2) {
@@ -38,15 +67,19 @@ public class MakeAmericaGreatAgain extends Ship {
         else if (i > 5) {
             this.move(arena, Direction.EAST);
         }
-        List<Ship> nearby = this.getNearbyShips(arena);
+       else this.fire(arena, 9, 2)
+       */
+       //this.move(arena, Direction.WEST);
+       
+        /*List<Ship> nearby = this.getNearbyShips(arena);
         if (nearby.size() > 0) {
             Ship first = nearby.get(0);
             Coord coord  = this.getShipCoord(arena, first);
             int x = coord.getX();
             int y = coord.getY(); 
-            this.fire(arena, x, y);
+            this.fire(arena, x, y); 
         // Fill in your strategy here
-         }
+         }*/
     }
     
     
