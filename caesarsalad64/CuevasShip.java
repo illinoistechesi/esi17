4074ec2bb41f06dd13e948ebcbc18a2752149dc2
Cesar Cuevas <ccuevas@hawk.iit.edu>
2017-07-20 01:48:32 +0000

    Modified CuevasShip

--- caesarsalad64/CuevasShip.java
+++ caesarsalad64/CuevasShip.java
@@ -6,15 +6,21 @@ import java.util.List;
  * Cuevas Ship
  * @author Your Name
  */
-public class CuevasShip extends Ship {
+public class CuevasShip extends Ship 
+{
     
-    public CuevasShip() {
+    public CuevasShip() 
+    { 
+    
+    //Plan A --Move around strategically [OFFENSE]
+    
         this.initializeName("Cuevas Ship");
         this.initializeOwner("Your Name");
-        this.initializeHull(1);
-        this.initializeFirepower(1);
-        this.initializeSpeed(1);
-        this.initializeRange(1);
+        this.initializeHull(4);      //Health
+        this.initializeFirepower(3); //Power
+        this.initializeSpeed(1);     //Speed
+        this.initializeRange(2);     //Range of visibility
+
     }
     
     /*
@@ -22,49 +28,54 @@ public class CuevasShip extends Ship {
      * @param arena (Arena) the battlefield for the match
      * @return void
      */
+    
     @Override
     public void doTurn(Arena arena) {
-        // Fill in your strategy here
+    // Fill in your strategy here
+    
+        Coord myShip = this.getCoord();
+        int a = myShip.getX();
+        
+        if (a > 2) 
+        {
+            this.move(arena, Direction.WEST);
+        }
+        else
+        {
+            this.move(arena, Direction.EAST);
+        }
     
     List<Ship> nearby = this.getNearbyShips(arena);
-    if(nearby.size() > 0)
+    // Loop over all the ships
+    for (Ship ship : nearby) 
     {
-        Ship first = nearby.get(0);
-        Coord coord = this.getShipCoord(arena, first); // gets the current location of the ship
+    System.out.println("Most near ship coords are: " + ship.getCoord() + " .");
+    }
+    
+    for (int i = 0; i < nearby.size(); i++)
+    {
+        Ship ship = nearby.get(i);
+        // Call the getTeam() method on any ship to get its team name
+        String myTeam = this.getTeam();
+        String theirTeam = ship.getTeam();
+        // To compare Strings, we have to use the special .equals() method
+        // It will return true if the strings are equal and false if they are not
+        if (theirTeam.equals(myTeam)) 
+        {
+        // Don't shoot!
+        } 
+        else 
+        {
+        // In the new version of battleship, you can get any ship's coordinate, even if it is out of your range
+        // But, since we used getNearbyShips(), all ships in this loop are in range
+       
+        Coord coord = ship.getCoord();
         int x = coord.getX();
         int y = coord.getY();
+        
+        // If you run out of firepower on a turn, you can still call fire(), but your ship won't actually fire
         this.fire(arena, x, y);
+            }
+        }
     }
-
-    if (x > 8) 
-    {
-        this.move(arena, Direction.WEST);
-    }
-    else if (x < 2) 
-    {
-        this.move(arena, Direction.EAST);
-    }
-    else if (y > 8) 
-    {
-        this.move(arena, Direction.NORTH);
-    }
-    else if (y < 2) 
-    {
-        this.move(arena, Direction.SOUTH);
-    }
-    else {
-        // make a list of all the location, and store it in a variable
-        Direction[] possibleMovement = {Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST};
-        
-        // get a random number and store it in a variable
-        Direction randomNumber = arena.getRandom().nextInt(4)
-        
-        // get a random movement by using the random number to access one possibleMovement 
-        this.move(arena, possibleMovement[randomNumber]);
-    }
-    
-    // ship using this instruction will fire at location (x: 0, y: 0) each turn
-    this.fire(arena, 0, 0);
-    }
-    
 }
