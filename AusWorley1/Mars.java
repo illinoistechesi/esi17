534483fd0982e1add1f431a44d7def56b51acea6
AusWorley1 <austin.worley@ymail.com>
2017-07-20 02:06:36 +0000

    Upgrade Mars Ship

--- AusWorley1/Mars.java
+++ AusWorley1/Mars.java
@@ -24,36 +24,83 @@ public class Mars extends Ship {
      */
     @Override
     public void doTurn(Arena arena) {
+       // Get all nearby ships
+    List<Ship> nearby = this.getNearbyShips(arena);
+// Loop over all the ships
+    for (int i = 0; i < nearby.size(); i++) {
+    Ship ship = nearby.get(i);
+    // Call the getTeam() method on any ship to get its team name    String myTeam = this.getTeam();
+    String myTeam = this.getTeam();
+    String theirTeam = ship.getTeam();
+    // To compare Strings, we have to use the special .equals() method
+    // It will return true if the strings are equal and false if they are not
+    if (theirTeam.equals(myTeam)) {
+        // Don't shoot!
+
+    } else {
+        // In the new version of battleship, you can get any ship's coordinate, even if it is out of your range
+        // But, snce we used getNearbyShips(), all ships in this loop are in range
+        Coord coord = ship.getCoord();        
+        int x = coord.getX();
+        int y = coord.getY();
+        // If you run out of firepower on a turn, you can still call fire(), but your ship won't actually fire
+  
+             if (nearby.size()>0) {
+            Ship a = nearby.get(0);
+           Coord coord2 = this.getShipCoord(arena, a);
+           int shipX = coord2.getX();
+           int shipY = coord2.getY();
+           this.move(arena, Direction.EAST);
+           this.fire(arena, x, y);
+           this.fire(arena, x, y);
+           this.fire(arena, x, y);
+           
+    }
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
         // Fill in your strategy here
-         List<Ship> nearby = this.getNearbyShips(arena);
-        Ship a;
+       //  List<Ship> nearby = this.getNearbyShips(arena);
+       // Ship a;
         //System.out.println(a.getHealth());
-        int size = nearby.size();
-        if (size>0) {
-            a = nearby.get(0);
-            Coord coord = this.getShipCoord(arena, a);
-            int shipX = coord.getX();
-            int shipY = coord.getY();
-            this.fire(arena, shipX, shipY);
-            this.fire(arena, shipX, shipY);
-            this.fire(arena, shipX, shipY);
-            this.move(arena, Direction.EAST);
-        }
-        else if (size == 0){
-            Coord coord = this.getCoord();
-            int x = coord.getX();
-            int maxX = arena.getXSize();
-            if (x == maxX){
-                this.move(arena, Direction.WEST);
-                
-            }else if (x < maxX){
-                this.move(arena, Direction.EAST);
-                
-            }
-            
-        }
+        //int size = nearby.size();
+       // if (size>0) {
+       //     a = nearby.get(0);
+        //    Coord coord = this.getShipCoord(arena, a);
+        //    int shipX = coord.getX();
+        //    int shipY = coord.getY();
+        //    this.fire(arena, shipX, shipY);
+        //    this.fire(arena, shipX, shipY);
+        //    this.fire(arena, shipX, shipY);
+        //    this.move(arena, Direction.EAST);
+       // }
+       // else if (size == 0){
+        //    Coord coord = this.getCoord();
+        //    int x = coord.getX();
+        //    int maxX = arena.getXSize();
+        //    if (x == maxX){
+        //        this.move(arena, Direction.WEST);
+        //        
+        //    }else if (x < maxX){
+        //        this.move(arena, Direction.EAST);
+        //        
+        //    }
+        //    
+       // }
         
-        
+    }
+    }
         
     //     Coord coord = this.getCoord();
     //     int x = coord.getX();
