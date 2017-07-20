4316236dc79a0b5e8fd12128d165e51868fe6572
ssoto7713 <ssoto7713@gmail.com>
2017-07-20 02:02:34 +0000

    team battle

---
+++ ssoto7713/SotoShip.java
@@ -0,0 +1,65 @@
+package esi17.ssoto7713;
+import battleship.core.*;
+import java.util.List;
+
+/*
+ * SotoShip
+ * @author Soto
+ */
+public class SotoShip extends Ship {
+    
+    public SotoShip() {
+        this.initializeName("SotoShip");
+        this.initializeOwner("Soto");
+        this.initializeHull(4);
+        this.initializeFirepower(2);
+        this.initializeSpeed(1);
+        this.initializeRange(3);
+    }
+    
+    /*
+     * Determines what actions the ship will take on a given turn
+     * @param arena (Arena) the battlefield for the match
+     * @return void
+     */
+    @Override
+    public void doTurn(Arena arena) {
+        // Fill in your strategy here
+           Coord me = this.getCoord();
+    int a = me.getX();
+    int b = me.getY();
+    
+    
+    if (a < 5) {
+        this.move(arena, Direction.EAST);
+        
+    }
+    else if (a > 5) {
+        this.move(arena, Direction.WEST);
+    }
+    
+        //gets neaby enemies
+        List<Ship> nearby = this.getNearbyShips(arena);
+// Loop over all the ships
+for (int i = 0; i < nearby.size(); i++) {
+    Ship ship = nearby.get(i);
+    // Call the getTeam() method on any ship to get its team name
+    String myTeam = this.getTeam();
+    String theirTeam = ship.getTeam();
+    // To compare Strings, we have to use the special .equals() method
+    // It will return true if the strings are equal and false if they are not
+    if (theirTeam.equals(myTeam)) {
+        // Don't shoot!
+    } else {
+        // In the new version of battleship, you can get any ship's coordinate, even if it is out of your range
+        // But, snce we used getNearbyShips(), all ships in this loop are in range
+        Coord coord = ship.getCoord();
+        int x = coord.getX();
+        int y = coord.getY();
+        // If you run out of firepower on a turn, you can still call fire(), but your ship won't actually fire
+        this.fire(arena, x, y);
+        this.fire(arena, x, y);
+    }
+}
+}
+}
