9248c36b8fa777d00f426a8d0320da5168afb954
ririgoyen99 <ririgoyen@hawk.iit.edu>
2017-07-22 01:35:21 +0000

    Immigrant NOW runs

--- ririgoyen99/Immigrant.java
+++ ririgoyen99/Immigrant.java
@@ -23,47 +23,27 @@ public class Immigrant extends Ship {
      * @return void
      */
     @Override
-    public void doTurn(Arena arena){
+   public void doTurn(Arena arena) {
+        this.move(arena, Direction.WEST);
+        this.move(arena, Direction.SOUTH);
+        this.move(arena, Direction.WEST);
+        this.move(arena, Direction.WEST);
+        this.move(arena, Direction.WEST);
+        this.move(arena, Direction.WEST);
+        this.move(arena, Direction.WEST);
         List<Ship> nearby = this.getNearbyShips(arena);
-// Loop over all the ships
-for (int i = 0; i < nearby.size(); i++) {
-    Ship ship = nearby.get(i);
-    // Call the getTeam() method on any ship to get its team name
-    String myTeam = this.getTeam();
-    String theirTeam = ship.getTeam();
-    // To compare Strings, we have to use the special .equals() method
-    // It will return true if the strings are equal and false if they are not
-    if (theirTeam.equals(myTeam)) {
-        // Don't shoot!
-    } else {
-        this.move(arena, Direction.WEST);
-        this.move(arena, Direction.WEST);
-        this.move(arena, Direction.WEST);
-        this.move(arena, Direction.WEST);
-        this.move(arena, Direction.SOUTH);
-        this.move(arena, Direction.SOUTH);
-        this.move(arena, Direction.NORTH);
-        this.move(arena, Direction.WEST);
-        if (nearby.size() > 0){
-         Ship first = nearby.get(0);
-         Coord coord = this.getShipCoord(arena, first);
-         int X = coord.getX();
-         int Y = coord.getY();
-         this.fire(arena, X, Y);
-        }}
-        if (nearby.size() > 0){
-         Ship first = nearby.get(1);
-         Coord coord = this.getShipCoord(arena, first);
-         int X = coord.getX();
-         int Y = coord.getY();
-         this.fire(arena, X, Y);
+        for (int i = 0; i < nearby.size(); i++) {
+        Ship ship = nearby.get(i);
+        String myTeam = this.getTeam();
+        String theirTeam = ship.getTeam();
+        if (theirTeam.equals(myTeam)) {
+    }   else {
+        Coord coord = ship.getCoord();
+        int x = coord.getX();
+        int y = coord.getY();
+        this.fire(arena, x, y);
+        }
     }
-    if (nearby.size() > 0){
-         Ship first = nearby.get(2);
-         Coord coord = this.getShipCoord(arena, first);
-         int X = coord.getX();
-         int Y = coord.getY();
-         this.fire(arena, X, Y);
-    }
-    }
-}}
+}
+}
+
