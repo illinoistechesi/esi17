e0926491a1c6e9901f53b6b7a449ff57c3a7575c
Cesar Cuevas <ccuevas@hawk.iit.edu>
2017-07-22 01:44:25 +0000

    Cuevas Ship 4.0

--- caesarsalad64/CuevasShip.java
+++ caesarsalad64/CuevasShip.java
@@ -6,21 +6,19 @@ import java.util.List;
  * Cuevas Ship
  * @author Your Name
  */
+ 
+//  https://github.com/illinoistechesi/battleship 
+ 
 public class CuevasShip extends Ship 
 {
-    
     public CuevasShip() 
     { 
-    
-    //Plan A --Move around strategically [OFFENSE]
-    
         this.initializeName("Cuevas Ship");
         this.initializeOwner("Your Name");
         this.initializeHull(4);      //Health
         this.initializeFirepower(3); //Power
         this.initializeSpeed(1);     //Speed
         this.initializeRange(2);     //Range of visibility
-
     }
     
     /*
@@ -32,52 +30,29 @@ public class CuevasShip extends Ship
     @Override
     public void doTurn(Arena arena) {
     // Fill in your strategy here
-    
-        Coord myShip = this.getCoord();
-        int a = myShip.getX();
         
-        if (a > 2) 
+    this.move(arena, Direction.WEST);
+    
+    List<Ship> nearby = this.getNearbyShips(arena);
+    for (int i = 0; i < nearby.size(); i++) 
+    {
+        Ship unknown = nearby.get(i);
+        boolean isOnMyTeam = this.isSameTeamAs(unknown);
+        if (isOnMyTeam) 
         {
-            this.move(arena, Direction.WEST);
+            System.out.println("This ship is on my team!");
+            //Don't Shoot
         }
         else
         {
-            this.move(arena, Direction.EAST);
-        }
-    
-    List<Ship> nearby = this.getNearbyShips(arena);
-    // Loop over all the ships
-    for (Ship ship : nearby) 
-    {
-    System.out.println("Most near ship coords are: " + ship.getCoord() + " .");
-    }
-    
-    for (int i = 0; i < nearby.size(); i++)
-    {
-        Ship ship = nearby.get(i);
-        // Call the getTeam() method on any ship to get its team name
-        String myTeam = this.getTeam();
-        String theirTeam = ship.getTeam();
-        // To compare Strings, we have to use the special .equals() method
-        // It will return true if the strings are equal and false if they are not
-        if (theirTeam.equals(myTeam)) 
-        {
-        // Don't shoot!
-        } 
-        else 
-        {
-        // In the new version of battleship, you can get any ship's coordinate, even if it is out of your range
-        // But, since we used getNearbyShips(), all ships in this loop are in range
-       
-        Coord coord = ship.getCoord();
-        int x = coord.getX();
-        int y = coord.getY();
+            System.out.println("This ship is an enemy.");
+            Coord location = this.getShipCoord(arena, unknown);
+            int x = location.getX();
+            int y = location.getY();
         
-        // If you run out of firepower on a turn, you can still call fire(), but your ship won't actually fire
-        this.fire(arena, x, y);
-        this.fire(arena, x, y);
-        this.fire(arena, x, y);
-        this.fire(arena, x, y);
+            this.fire(arena, x, y);
+            
+            System.out.println("Fired at: " + x + ", " + y); 
             }
         }
     }
