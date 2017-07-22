88fa2de7450dd416fbd51763daef5d93b4dd82dd
Cameron Guy <cguy1@hawk.iit.edu>
2017-07-22 01:44:23 +0000

    shiptokill

--- cguy1/Pirateship.java
+++ cguy1/Pirateship.java
@@ -11,10 +11,10 @@ public class Pirateship extends Ship {
     public Pirateship ()  {
         this.initializeName("Pirateship");
         this.initializeOwner("cguy1");
-        this.initializeHull(3);
-        this.initializeFirepower(3);
+        this.initializeHull(1);
+        this.initializeFirepower(4);
         this.initializeSpeed(0);
-        this.initializeRange(4);
+        this.initializeRange(5);
     }
     
     /*
@@ -25,17 +25,34 @@ public class Pirateship extends Ship {
     @Override
     public void doTurn(Arena arena) {
         this.move(arena, Direction.EAST);
-        List<Ship> nearby = this.getNearbyShips(arena);
-        if (nearby.size() > 0) {
+        //List<Ship> nearby = this.getNearbyShips(arena);
+       
+        //if (nearby.size() > 0) {
            
-        Ship first = nearby.get(0);
-        Coord coord = this.getShipCoord(arena, first);
-        int x = coord.getX();
-        int y = coord.getY();
-        this.fire(arena, x, y);
+        //Ship first = nearby.get(0);
+        //Coord coord = this.getShipCoord(arena, first);
+        //int x = coord.getX();
+        //int y = coord.getY();
+        //this.fire(arena, x, y);
+        
+        List<Ship> nearby = this.getNearbyShips(arena);
+        for (int i = 0; i < nearby.size(); i++) {
+            Ship unknown = nearby.get(i);
+            boolean isOnMyTeam = this.isSameTeamAs(unknown);
             
+            if (isOnMyTeam) {
+                //System.out.println("This ship is on my team!");
+            }      
+            else {
+                Coord coord = this.getShipCoord(arena, unknown);
+                int x = coord.getX();
+                int y = coord.getY();
+                this.fire(arena, x, y);
+            }
         }
         
+            
     }
-    
+        
 }
+    
