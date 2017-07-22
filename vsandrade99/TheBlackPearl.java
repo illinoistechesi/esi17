7b324f2c22b69972104a80b3a38df6c5b7e2bc44
Vladimir Andrade <vandrade@hawk.iit.edu>
2017-07-22 01:39:38 +0000

    Friendly fire

--- vsandrade99/TheBlackPearl.java
+++ vsandrade99/TheBlackPearl.java
@@ -1,18 +1,18 @@
 package esi17.vsandrade99;
-import battleship.core.*;
+import core.*;
 import java.util.List;
 
 /*
- * TheBlackPearl
- * @Vladimir
+ * BlackPearl
+ * Vladimir
  */
 public class TheBlackPearl extends Ship {
     
     public TheBlackPearl() {
-        //max points availible is 10
-        this.initializeName("TheBlackPearl");
+        //max points availible is 11
+        this.initializeName("BlackPearl");
         this.initializeOwner("Vladimir");
-        this.initializeHull(3);
+        this.initializeHull(5);
         this.initializeFirepower(3);
         this.initializeSpeed(1);
         this.initializeRange(3);
@@ -29,20 +29,50 @@ public class TheBlackPearl extends Ship {
         //int goalX=0;
         //int goalY=0;
         
-        this.move(arena, Direction.WEST);
-        this.move(arena, Direction.WEST);
-        this.move(arena, Direction.NORTH);
-        this.move(arena, Direction.NORTH);
-        this.fire(arena, 3,1);
-        this.move(arena, Direction.EAST);
-        this.fire(arena, 4,0);
-        this.move(arena, Direction.EAST);
-        this.fire(arena, 5,0);
-        this.move(arena, Direction.EAST);
-        this.fire(arena, 6,1);
-        this.move(arena, Direction.SOUTH);
-        this.move(arena, Direction.SOUTH);
-        this.move(arena, Direction.WEST);
+        arena.move(this, Direction.EAST);
+        arena.move(this, Direction.EAST);
+        arena.move(this, Direction.EAST);
+        arena.fire(this, 7,1);
+        arena.move(this, Direction.NORTH);
+        arena.fire(this, 7,2);
+        arena.move(this, Direction.SOUTH);
+        arena.fire(this, 6,2);
+        arena.move(this, Direction.SOUTH);
+        arena.fire(this, 7,3);
+        arena.move(this, Direction.NORTH);
+        arena.move(this, Direction.WEST);
+        arena.move(this, Direction.WEST);
     }
+    /*
+     * Example of how not to shoot your teammates
+    */
+    // Get all nearby ships
+    List<Ship> nearby = this.getNearbyShips(arena);
+    // Loop over all the ships
+    for (int i = 0; i < nearby.size(); i++) {
+        Ship ship = nearby.get(i);
+        String myTeam = this.getTeam();
+        String theirTeam = ship.getTeam();
+        if (theirTeam.equals(myTeam)) {
+            // Don't shoot!
+        } else {
+            Coord coord = ship.getCoord();
+            int x = coord.getX();
+            int y = coord.getY();
+            int myX = this.getCoord().getX();
+            int myY = this.getCoord().getY();
+        
+            if (myX>x) {
+                this.move(arena,Direction.WEST);
+            } else if (myX<x) {
+                this.move(arena,Direction.EAST);
+            } else if (myY>y) {
+                this.move(arena,Direction.NORTH);
+            } else this.move(arena,Direction.SOUTH);
+        
+            this.fire(arena, x, y);
+        }
+    }
+    
     
 }
