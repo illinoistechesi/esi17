5032de1eae98c99ae4c133864cd566f2e6181b89
Nick <nlambros@hahwk.iit.edu>
2017-07-27 01:42:43 +0000

    {shoutout ot my boi jamez hustling to feed his daughter those capre suns}

--- Nickthegreat/MoistNoodle.java
+++ Nickthegreat/MoistNoodle.java
@@ -11,10 +11,10 @@ public class MoistNoodle extends Ship {
     public MoistNoodle() {
         this.initializeName("MoistNoodle");
         this.initializeOwner("Nick");
-        this.initializeHull(2);
+        this.initializeHull(1);
         this.initializeFirepower(2);
         this.initializeSpeed(1);
-        this.initializeRange(5);
+        this.initializeRange(6);
     }
  
     /*
@@ -28,13 +28,15 @@ public class MoistNoodle extends Ship {
         Coord me = this.getCoord();
         int a = me.getX();
         int b = me.getY();
-    
-    
+            
         
-        if (a < 7) {
-        this.move(arena, Direction.EAST);
-        this.move(arena, Direction.EAST);
-        }
+         
+         
+        
+        //if (a < 7) {
+        //this.move(arena, Direction.EAST);
+        //this.move(arena, Direction.EAST);
+        //}
     
         //gets neaby enemies
         List<Ship> nearby = this.getNearbyShips(arena);
@@ -55,39 +57,20 @@ for (int i = 0; i < nearby.size(); i++) {
         Coord enemy = ship.getCoord();
         int x = enemy.getX();
         int y = enemy.getY();
-        
         this.fire(arena, x, y);
         this.fire(arena, x, y);
-        
-       
        // for (int firecount=0;firecount<6;firecount++){
     //   if ship.getHealth()>0;
       //  this.fire(arena, x, y);
         //else
         //for (int i = 0; i < nearby.size(); i++) {
-    
-    
-    ;
+    this.move(arena, Direction.SOUTH);
         
         }
         // If you run out of firepower on a turn, you can still call fire(), but your ship won't actually fire
 
     }
 }
-        
-        
-        
-        
-
-    
-        
-        
-        
-        
-
-
-        
-        
         // Fill in your strategy here
     
     
