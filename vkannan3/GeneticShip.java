ca9fd6e606f7d833ff127c8e22acd5de6ebff202
Vinesh Kannan <vingkan@gmail.com>
2017-07-28 05:23:39 +0000

    Train genetic ship that averages 9 kills

--- vkannan3/GeneticShip.java
+++ vkannan3/GeneticShip.java
@@ -1,8 +1,10 @@
 package esi17.vkannan3;
 import battleship.core.*;
+import battleship.games.*;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Arrays;
 import java.io.PrintStream;
 
 /*
@@ -10,6 +12,26 @@ import java.io.PrintStream;
  * @author Your Name
  */
 public class GeneticShip extends Ship {
+    
+    public static void main(String[] args) {
+        int seed = GeneticTrial.getRandom().nextInt(10);
+        if (args.length >= 1) {
+            seed = Integer.parseInt(args[0]);
+        }
+        System.out.println("Battle Seed: " + seed);
+        int[] chromosome = {1, 3, 0, 6, 3, 0, 0, 1, 1};
+        CustomBattle battle = GeneticTrial.runTrial(chromosome, seed);
+        Arena arena = battle.getArena();
+        GeneticShip geneticShip = GeneticTrial.getGeneticShip(arena);
+        System.out.println(geneticShip);
+        System.out.println(geneticShip.getPhenotype());
+        int kills = GeneticTrial.getKillsByShip(arena, geneticShip);
+        int survived = geneticShip.getTurnsSurvived();
+        int turns = arena.getTurn();
+        System.out.println("Sunk " + kills + " ships.");
+        System.out.println("Survived " + survived + "/" + turns + " turns.");
+        GeneticTrial.showBattleResults(battle);
+    }
     
     private static int[] CHROMOSOME;
     private static final int[] GENE_MAX = {9, 9, 9, 9, 4, 1, 4, 1, 1};
@@ -66,37 +88,38 @@ public class GeneticShip extends Ship {
         System.setOut(GeneticShip.out);
         this.turnsSurvived++;
         List<Ship> nearby = this.getNearbyShips(arena);
-        GeneticShip self = this;
-        Collections.sort(nearby, new Comparator<Ship>() {
-            public int compare(Ship s1, Ship s2) {
-                int comparison = self.getComparison(s1, s2, PRIORITY_A_GENE, PRIORITY_A_SIGN_GENE);
-                if (comparison == 0) {
-                    comparison = self.getComparison(s1, s2, PRIORITY_B_GENE, PRIORITY_B_SIGN_GENE);
-                }
-                return comparison;
-            } 
-        });
-        switch (this.getGene(FIRE_PATTERN_GENE)) {
-            case 0: // Eliminate
-                Ship first = nearby.get(0);
-                Coord coord = first.getCoord();
-                for (int f = 0; f < this.getFirepower(); f++) {
-                    this.fire(arena, coord.getX(), coord.getY());
-                }
-                break;
-            case 1:  // Spread
-                for (int s = 0; s < this.getFirepower(); s++) {
-                    if (s < nearby.size()) {
+        if (nearby.size() > 0) {
+            GeneticShip self = this;
+            Collections.sort(nearby, new Comparator<Ship>() {
+                public int compare(Ship s1, Ship s2) {
+                    int comparison = self.getComparison(s1, s2, PRIORITY_A_GENE, PRIORITY_A_SIGN_GENE);
+                    if (comparison == 0) {
+                        comparison = self.getComparison(s1, s2, PRIORITY_B_GENE, PRIORITY_B_SIGN_GENE);
+                    }
+                    return comparison;
+                } 
+            });
+            switch (this.getGene(FIRE_PATTERN_GENE)) {
+                case 0: // Eliminate
+                    Ship first = nearby.get(0);
+                    Coord coord = first.getCoord();
+                    for (int f = 0; f < this.getFirepower(); f++) {
+                        this.fire(arena, coord.getX(), coord.getY());
+                    }
+                    break;
+                case 1:  // Spread
+                    int s = 0;
+                    while (this.getRemainingShots() > 0) {
                         Ship target = nearby.get(s);
                         Coord loc = target.getCoord();
-                        this.fire(arena, loc.getX(), loc.getY());   
-                    } else {
-                        break;
+                        this.fire(arena, loc.getX(), loc.getY());
+                        s++;
+                        s = s % nearby.size();
                     }
-                }
-                break;
-            default:
-                break;
+                    break;
+                default:
+                    break;
+            }
         }
     }
     
@@ -172,6 +195,51 @@ public class GeneticShip extends Ship {
     
     private int getGene(int index) {
         return GeneticShip.getChromosome()[index];
+    }
+    
+    public String getPhenotype() {
+        String res = "Genetic Ship";
+        res += "\n- " + "Genes: " + Arrays.toString(this.getChromosome());
+        res += "\n- " + "Hull: " + this.getGene(HULL_GENE);
+        res += "\n- " + "Firepower: " + this.getGene(FIREPOWER_GENE);
+        res += "\n- " + "Speed: " + this.getGene(SPEED_GENE);
+        res += "\n- " + "Firepower: " + this.getGene(RANGE_GENE);
+        String priorityA = "???";
+        String priorityB = "???";
+        switch (this.getGene(PRIORITY_A_GENE)) {
+            case 0: // Distance
+                priorityA = "distance";
+                priorityB = "distance";
+                break;
+            case 1: // Health
+                priorityA = "health";
+                priorityB = "health";
+                break;
+            case 2: // Firepower
+                priorityA = "firepower";
+                priorityB = "firepower";
+                break;
+            case 3: // Speed
+                priorityA = "speed";
+                priorityB = "speed";
+                break;
+            case 4: // Range
+                priorityA = "range";
+                priorityB = "range";
+                break;
+            default:
+                break;
+        }
+        String sortA = this.getGene(PRIORITY_A_SIGN_GENE) == 0 ? "decreasing" : "increasing";
+        String sortB = this.getGene(PRIORITY_B_SIGN_GENE) == 0 ? "decreasing" : "increasing";
+        res += "\n- " + "Sort nearby enemies by " + sortA + " " + priorityA + ",";
+        res += "\n- " + "Then sort by " + sortB + " " + priorityB + ",";
+        if (this.getGene(FIRE_PATTERN_GENE) == 0) {
+            res += "\n- " + "Then focus shots on the first ship.";   
+        } else {
+            res += "\n- " + "Then distribute shots among the ships.";
+        }
+        return res;
     }
     
 }
--- vkannan3/GeneticTrial.java
+++ vkannan3/GeneticTrial.java
@@ -4,11 +4,22 @@ import battleship.games.*;
 import battleship.ships.*;
 import java.util.*;
 
+/*
+ * $ javac esi17/vkannan3/GeneticTrial.java
+ * $ java esi17.vkannan3.GeneticTrial 14
+ * $ javac esi17/vkannan3/GeneticShip.java
+ * $ java esi17.vkannan3.GeneticShip
+ */
 public class GeneticTrial {
     
     private static final String INSTRUCTOR_TEAM = "Instructors";
     private static final String STUDENT_TEAM = "Students";
     private static Random RANDOM = new Random();
+    private static final int TRIALS = 5;
+    private static final int POPULATION_SIZE = 10;
+    private static final int GENERATIONS = 4;
+    private static final double GENE_MUTATION_RATE = 0.10;
+    private static final double PROPERTY_MUTATION_RATE = 0.05;
     
     public static void main(String[] args) {
         int simulationSeed = (int) Math.floor(Math.random() * 100);
@@ -20,31 +31,204 @@ public class GeneticTrial {
         // Use Simulation Seed = 4
         // $ java esi17.vkannan3.GeneticTrial 4
         
-        int[] chromosome = getRandomChromosome();
-        for (int i = 0; i < 10; i++) {
+        Helper.disableWrites();
+        
+        System.out.println("\r\rInitial Population");
+        Map<int[], Double> population = new HashMap<int[], Double>();
+        for (int p = 0; p < POPULATION_SIZE; p++) {
+            int[] chromosome = getRandomChromosome();
+            double fitness = getMeanFitness(chromosome);
+            //System.out.println(p + ": " + Arrays.toString(chromosome) + " -> " + fitness);   
+            population.put(chromosome, fitness);
+        }
+        
+        for (int n = 0; n < GENERATIONS; n++) {
+            System.out.println("\r\rGeneration " + n);
+            population = getNextPopulation(population);
+        }
+        
+        System.out.println("Generation " + GENERATIONS);
+        for (Map.Entry<int[], Double> candidate : population.entrySet()) {
+            int[] chromosome = candidate.getKey();
+            double fitness = candidate.getValue();
+            System.out.println(Arrays.toString(chromosome) + " -> " + fitness);   
+        }
+        
+        
+    }
+    
+    public static Map<int[], Double> getNextPopulation(Map<int[], Double> population) {
+        Map<int[], Double> nextPopulation = new HashMap<int[], Double>();
+        int[] parent1 = chooseParent(population, 1);
+        int[] parent2 = chooseParent(population, 2);
+        int[] parent3 = chooseParent(population, 3);
+        int[] parent4 = chooseParent(population, 4);
+        double fitness1 = population.get(parent1);
+        double fitness2 = population.get(parent2);
+        double fitness3 = population.get(parent3);
+        double fitness4 = population.get(parent4);
+        //System.out.println(Arrays.toString(parent1) + " <-> " + Arrays.toString(parent2));
+        int halfSize = POPULATION_SIZE / 2;
+        for (int c = 0; c < halfSize; c++) {
+            int[] child1 = crossover(parent1, fitness1, parent4, fitness4);
+            child1 = mutate(child1);
+            double childFitness1 = getMeanFitness(child1);
+            //System.out.println("Child " + c + ": " + Arrays.toString(child) + " -> " + childFitness);
+            nextPopulation.put(child1, childFitness1);
+            //
+            int[] child2 = crossover(parent2, fitness2, parent3, fitness3);
+            child2 = mutate(child2);
+            double childFitness2 = getMeanFitness(child2);
+            nextPopulation.put(child2, childFitness2);
+        }
+        return nextPopulation;
+    }
+    
+    public static int[] mutate(int[] child) {
+        int[] mutated = new int[child.length];
+        for (int g = 0; g < child.length; g++) {
+            if (g >= 4) {
+                double rand = getRandom().nextDouble();
+                if (rand > GENE_MUTATION_RATE) {
+                    int geneMax = GeneticShip.getGeneMax(g);
+                    mutated[g] = getRandom().nextInt(geneMax + 1);
+                } else {
+                    mutated[g] = child[g];
+                }
+            } else {
+                mutated[g] = child[g];
+            }
+        }
+        double rand = getRandom().nextDouble();
+        if (rand > PROPERTY_MUTATION_RATE) {
+            int baseIndex = getRandom().nextInt(4);
+            int swapIndex = getRandom().nextInt(4);
+            while (baseIndex == swapIndex) {
+                baseIndex = getRandom().nextInt(4);
+                swapIndex = getRandom().nextInt(4);
+            }
+            mutated[baseIndex] = child[swapIndex];
+            mutated[swapIndex] = child[baseIndex];
+        }
+        return mutated;
+    }
+    
+    public static int[] crossover(int[] p1, double f1, int[] p2, double f2) {
+        int[] child = new int[p1.length];
+        for (int i = 0; i < p1.length; i++) {
+            double random = getRandom().nextDouble();
+            if (random > 0.5) {
+                child[i] = p2[i];
+            } else {
+                child[i] = p1[i];
+            }
+        }
+        double propsRand = getRandom().nextDouble();
+        int[] propsParent = new int[p1.length];
+        //if (f2 > f1) {
+        if (propsRand > 0.5) {
+            propsParent = p2;
+        } else {
+            propsParent = p1;
+        }
+        for (int p = 0; p < 4; p++) {
+            child[p] = propsParent[p];
+        }
+        return child;
+    }
+    
+    public static int[] chooseParent(Map<int[], Double> population, int num) {
+        int passed = 0;
+        double sum = 0;
+        for (Map.Entry<int[], Double> candidate : population.entrySet()) {
+            int[] chromosome = candidate.getKey();
+            Double fitness = candidate.getValue();
+            sum += fitness;
+        }
+        double partial = getRandom().nextDouble() * sum;
+        while (passed < num) {
+            for (Map.Entry<int[], Double> candidate : population.entrySet()) {
+                int[] chromosome = candidate.getKey();
+                Double fitness = candidate.getValue();
+                partial += fitness;
+                if (partial >= sum) {
+                    passed++;
+                    if (passed >= num) {
+                        return chromosome;   
+                    }
+                }
+            }   
+        }
+        return null;
+    }
+    
+    public static double getMeanFitness(int[] chromosome) {
+        double sum = 0;
+        double total = 0;
+        for (int i = 0; i < TRIALS; i++) {
             int seed = i;
             CustomBattle battle = runTrial(chromosome, seed);
             Arena arena = battle.getArena();
             double fitness = getFitness(arena);
-            System.out.println(Arrays.toString(chromosome) + " -> " + fitness);
+            sum += fitness;
+            total++;
+            //System.out.println(Arrays.toString(chromosome) + " -> " + fitness);
         }
-        
+        double mean = sum / total;
+        return mean;
     }
     
     public static double getFitness(Arena arena) {
         double fitness = 0;
         GeneticShip ship = getGeneticShip(arena);
         if (hasValidProperties(ship)) {
-            int turns = ship.getTurnsSurvived();
+            /*int turns = ship.getTurnsSurvived();
             int kills = getKillsByShip(arena, ship);
             int killsKillsSum = getKillsOfKills(arena, ship);
             fitness = turns;
             fitness = kills;
-            fitness = killsKillsSum;
+            fitness = killsKillsSum;*/
+            //fitness = getFinalScoreRatio(arena);
+            double ratio = getFinalScoreRatio(arena);
+            double kills = (double) getKillsByShip(arena, ship);
+            double turns = (double) ship.getTurnsSurvived();
+            fitness = kills;//ratio * kills;
         } else {
             fitness = 0;
         }
         return fitness;
+    }
+    
+    public static double getFinalScoreRatio(Arena arena) {
+        String instructorTeam = INSTRUCTOR_TEAM;
+        String studentTeam = STUDENT_TEAM;
+        Map<String, Integer> scoreMap = new HashMap<String, Integer>();
+        scoreMap.put(instructorTeam, 0);
+        scoreMap.put(studentTeam, 0);
+        for (Ship ship : arena.getAllSpawnedShips()) {
+            if (ship.isSunk()) {
+                if (ship.getTeam().equals(instructorTeam)) {
+                    int ss = scoreMap.get(studentTeam);
+                    ss++;
+                    scoreMap.put(studentTeam, ss);
+                } else {
+                    int is = scoreMap.get(instructorTeam);
+                    is++;
+                    scoreMap.put(instructorTeam, is);
+                }
+            }
+        }
+        double sfs = (double) scoreMap.get(studentTeam);
+        double ifs = (double) scoreMap.get(instructorTeam);
+        String result = "Match is a Draw.";
+        if (sfs > ifs) {
+            result = "Students win.";
+        } else if (sfs < ifs) {
+            result = "Instructors win.";
+        }
+        double ratio = ifs / sfs;
+        //System.out.println(result + " -> " + ratio);
+        return ratio;
     }
     
     public static int getKillsOfKills(Arena arena, Ship attacker) {
@@ -98,8 +282,56 @@ public class GeneticTrial {
             int geneMax = GeneticShip.getGeneMax(g);
             chromosome[g] = getRandom().nextInt(geneMax + 1);
         }
+        // Set property genes
+        int total = 10;
+        int[] splitPoints = new int[4];
+        for (int r = 0; r < splitPoints.length; r++) {
+            splitPoints[r] = getRandom().nextInt(total);
+        }
+        splitPoints[3] = 10;
+        Arrays.sort(splitPoints);
+        int lastSplit = 0;
+        for (int s = 0; s < splitPoints.length; s++) {
+            int thisSplit = splitPoints[s];
+            int val = thisSplit - lastSplit;
+            lastSplit = thisSplit;
+            chromosome[s] = val;
+        }
+        int sum = 0;
+        int[] props = new int[4];
+        for (int q = 0; q < 4; q++) {
+            int gene = chromosome[q];
+            props[q] = gene;
+            sum += gene;
+        }
+        //System.out.println("\n" + Arrays.toString(props) + " = " + sum + "\n");
+        //
         return chromosome;
     }
+    
+    /*  OLD PROPERTY GENE RANDOMIZATION
+        int propsLeft = 4;
+        int index = 0;
+        int total = 10;
+        int sum = 0;
+        //String res = "";
+        while (propsLeft > 1) {
+            int val = getRandom().nextInt(total - propsLeft);
+            if (index != 2) {
+                val += 1; // Prevent properties other than speed from being zero
+            }
+            chromosome[index] = val;
+            //res += val + " + ";
+            sum += val;
+            total -= val;
+            propsLeft--;
+            index++;
+        }
+        chromosome[index] = total; // Always use up the full 10 points
+        //res += total + " + ";
+        sum += total;
+        //System.out.println("\n" + res + " = " + sum + "\n");
+    */
     
     public static GeneticShip getGeneticShip(Arena arena) {
         for (Ship ship : arena.getAllSpawnedShips()) {
@@ -209,7 +441,9 @@ public class GeneticTrial {
         
     }
     
-    public static void showBattleResults(CustomBattle battle, String instructorTeam, String studentTeam) {
+    public static void showBattleResults(CustomBattle battle) {
+        String instructorTeam = INSTRUCTOR_TEAM;
+        String studentTeam = STUDENT_TEAM;
         Arena arena = battle.getArena();
         Map<String, Integer> scoreMap = new HashMap<String, Integer>();
         scoreMap.put(instructorTeam, 0);
